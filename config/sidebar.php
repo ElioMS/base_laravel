4edbd7ba08d70dbecffff8dff56dcd4f101f2109
eliogsolis <elio.gsolis@gmail.com>
2017-04-28 16:43:49 -0500

    actualizacion sidebar

--- config/sidebar.php
+++ config/sidebar.php
@@ -2,34 +2,20 @@
 	
 return[
 	'menu' => [
-      // [
-      //   'type' => 'simple',
-      //   'text' => 'Categories',
-      //   'url'  => 'categories.index',
-      //   'ico'  => 'edit'
-      // ],
 
-      [
+    [
         'type' => 'simple',
         'text' => 'Subscriptions',
         'url'  => 'subscriptions.index',
         'ico'  => 'edit'
-      ],
+    ],
 
-      [
+    [
         'type' => 'simple',
         'text' => 'Contacts',
         'url'  => 'contacts.index',
         'ico'  => 'edit'
-      ],
-
-      // [
-      //   'type' => 'simple',
-      //   'text' => 'Subcategories',
-      //   'url'  => 'subcategories.index',
-      //   'ico'  => 'edit'
-      // ],
-
+    ],
 
     [
         'type' => 'tree',
@@ -52,50 +38,6 @@ return[
         ]
     ],
 
-
-        // [
-        //     'type' => 'simple',
-        //     'text' => 'Galleries',
-        //     'url'  => '',
-        //     'ico'  => 'sound-dolby'
-        // ],
-
-        // [
-        //     'type' => 'simple',
-        //     'text' => 'News',
-        //     'url'  => '',
-        //     'ico'  => 'export'
-        // ],
-
-        // [
-        //     'type' => 'simple',
-        //     'text' => 'Contact',
-        //     'url'  => '',
-        //     'ico'  => 'earphone'
-        // ],
-
-        // [
-        //  'type' => 'tree',
-        //  'text' => 'Productos',
-        //  'ico'  => 'shopping-cart',
-        //  'childs' => [
-        //         [
-        //           'text' => 'Categories',
-        //           'url' => '',
-        //           'ico' => 'fa fa-groups'
-        //         ],
-        //         [
-        //           'text' => 'Subcategories',
-        //           'url' => '',
-        //           'ico' => 'fa fa-home'
-        //         ],
-        //         [
-        //           'text' => 'Products',
-        //           'url' => '',
-        //           'ico' => 'fa fa-envelope'
-        //         ],
-        //     ]
-        // ],
 	]
 ]
 ?>
